322498c4c9798aaa91987c613c73072fa44272fd
tvaisanen <vaisanen.toni@gmail.com>
2019-07-30 23:09:57 +0300

    Fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -18,7 +18,6 @@ const Main = styled.main`
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  border: 1px solid black;
   width: 100%;
   height: 100%;
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,15 +5,10 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IconEnter = styled.svg``
-
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>t.vaisanen</h1>
-    <svg height="100" height="100">
-      <circle cx="50" cy="50" r="40" />
-    </svg>
   </Layout>
 )
 
